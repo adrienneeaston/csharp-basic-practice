0602fd155faa0b251e51b74b704e9cc5e577f1f0
Adrienne <adrienneeaston@gmail.com>
2021-08-08 17:20:04 -0700

    string length

--- Practice.cs
+++ Practice.cs
@@ -2,3 +2,12 @@ Console.WriteLine("Hello World!");
 
 string aFriend = "Adrienne";
 Console.WriteLine($"Hello {aFriend}");
+
+string firstFriend = "Adrienne";
+string secondFriend = "Chris";
+Console.WriteLine($"My Friends are {firstFriend} and {secondFriend}");
+
+string firstFriend = "Adrienne";
+string secondFriend = "Chris";
+Console.WriteLine($"My Friend {firstFriend} has {firstFriend.Length} letters.");
+Console.WriteLine($"My Friend {secondFriend} has {secondFriend.Length} letters.");
