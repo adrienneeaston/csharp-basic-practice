b14c62e4343352640f19f6e98e3e1403502ab176
Adrienne <adrienneeaston@gmail.com>
2021-08-09 23:16:46 -0700

    lists with add, remove, refer to index, and count list items

--- Practice.cs
+++ Practice.cs
@@ -188,3 +188,47 @@ for (int number = 1; number < 21; number++)
   }
 }
 Console.WriteLine($"The sum is {sum}");
+
+// lists/arrays/collections
+var names = new List<string> { "Adrienne", "Ana", "Felipe" };
+foreach (var name in names)
+{
+    Console.WriteLine($"Hello {name.ToUpper()}!");
+}
+
+// add, remove
+var names = new List<string> { "Adrienne", "Ana", "Felipe" };
+foreach (var name in names)
+{
+    Console.WriteLine($"Hello {name.ToUpper()}!");
+}
+
+Console.WriteLine();
+names.Add("Maria");
+names.Add("Bill");
+names.Remove("Ana");
+foreach (var name in names)
+{
+    Console.WriteLine($"Hello {name.ToUpper()}!");
+}
+
+// refer to index value, count items
+var names = new List<string> { "Adrienne", "Ana", "Felipe" };
+foreach (var name in names)
+{
+    Console.WriteLine($"Hello {name.ToUpper()}!");
+}
+
+Console.WriteLine();
+names.Add("Maria");
+names.Add("Bill");
+names.Remove("Ana");
+foreach (var name in names)
+{
+    Console.WriteLine($"Hello {name.ToUpper()}!");
+}
+
+Console.WriteLine($"My name is {names[0]}.");
+Console.WriteLine($"I've added {names[2]} and {names[3]} to the list.");
+
+Console.WriteLine($"The list has {names.Count} people in it");
