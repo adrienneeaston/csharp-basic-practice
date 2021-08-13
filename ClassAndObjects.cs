7951f724607cbc0ce305d11a264d3708691aafca
Adrienne <adrienneeaston@gmail.com>
2021-08-12 23:19:14 -0700

    bank account calculating deposits and withdrawals

--- ClassAndObjects.cs
+++ ClassAndObjects.cs
@@ -11,6 +11,11 @@ namespace MySuperBank
         {
             var account = new BankAccount("Adrienne", 20000);
             Console.WriteLine($"Account {account.Number} was created for {account.Owner} has {account.Balance}.");
+
+            account.MakeWithdrawal(500, DateTime.Now, "Rent payment");
+            Console.WriteLine(account.Balance);
+            account.MakeDeposit(100, DateTime.Now, "Friend paid me back");
+            Console.WriteLine(account.Balance);
         }
     }
 }
@@ -26,20 +31,76 @@ namespace MySuperBank
     {
         public string Number { get; }
         public string Owner { get; set; }
-        public decimal Balance { get; }
+        public decimal Balance
+        {
+            get
+            {
+                decimal balance = 0;
+                foreach (var item in allTransactions)
+                {
+                    balance += item.Amount;
+                }
+
+                return balance;
+            }
+        }
+
+        private static int accountNumberSeed = 1234567890;
+
+        private List<Transaction> allTransactions = new List<Transaction>();
 
         public BankAccount(string name, decimal initialBalance)
         {
-            Owner = name;
-            Balance = initialBalance;
+            this.Owner = name;
+            MakeDeposit(initialBalance, DateTime.Now, "Initial balance");
+            this.Number = accountNumberSeed.ToString();
+            accountNumberSeed++;
         }
 
         public void MakeDeposit(decimal amount, DateTime date, string note)
         {
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of deposit must be positive");
+            }
+            var deposit = new Transaction(amount, date, note);
+            allTransactions.Add(deposit);
         }
 
         public void MakeWithdrawal(decimal amount, DateTime date, string note)
         {
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of withdrawal must be positive");
+            }
+            if (Balance - amount < 0)
+            {
+                throw new InvalidOperationException("Not sufficient funds for this withdrawal");
+            }
+            var withdrawal = new Transaction(-amount, date, note);
+            allTransactions.Add(withdrawal);
+        }
+    }
+}
+
+// Transaction.cs
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MySuperBank
+{
+    class Transaction
+    {
+        public decimal Amount { get; }
+        public DateTime Date { get; }
+        public string Notes { get; }
+
+        public Transaction(decimal amount, DateTime date, string note)
+        {
+            this.Amount = amount;
+            this.Date = date;
+            this.Notes = note;
         }
     }
 }
