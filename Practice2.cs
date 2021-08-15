00e87f2d8aa1b8da9dca702b5cee04b3b3fa64f3
Adrienne <adrienneeaston@gmail.com>
2021-08-15 16:05:20 -0700

    concatonation

--- Practice2.cs
+++ Practice2.cs
@@ -16,3 +16,12 @@ Console.WriteLine(@"c:\invoices");
 // Nihon no seikyū-sho o seisei suru ni wa:
 Console.Write("\n\n\u65e5\u672c\u306e\u8acb\u6c42\u66f8\u3092\u751f\u6210\u3059\u308b\u306b\u306f\uff1a\n\t");
 Console.WriteLine(@"c:\invoices\app.exe -j");
+
+//concatenation
+string firstName = "Bob";
+string message = "Hello " + firstName;
+Console.WriteLine(message);
+
+string firstName = "Bob"; 
+string greeting = "Hello";
+Console.WriteLine(greeting + " " + firstName + "!");
