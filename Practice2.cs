4164a836ebff64d66413fd984ade8487d8f37229
Adrienne <adrienneeaston@gmail.com>
2021-08-16 23:29:27 -0700

    foreach

--- Practice2.cs
+++ Practice2.cs
@@ -209,3 +209,82 @@ else
 {
     Console.WriteLine("You win a kitten!");
 }
+
+
+// project - random days left in subscription, if/else
+Random random = new Random();
+int daysUntilExpiration = random.Next(12);
+int discountPercentage = 0;
+
+if (daysUntilExpiration <= 10) 
+{
+    if (daysUntilExpiration == 0)
+    {
+        Console.WriteLine("You subscription has expired.");
+    }
+    else if (daysUntilExpiration == 1) 
+    {
+        discountPercentage = 20;
+        Console.WriteLine("Your subscription expires within a day!");
+        Console.WriteLine($"Renew now and save {discountPercentage}%!");
+    } 
+    else if (daysUntilExpiration <= 5)
+    {
+        discountPercentage = 10;
+        Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.");
+        Console.WriteLine($"Renew now and save {discountPercentage}%!");
+    }
+    else
+    {
+        Console.WriteLine("Your subscription will expire soon. Renew now!");
+    }
+} 
+
+// creating arrays
+// string[] fraudulentOrderIDs = new string[3];
+
+// fraudulentOrderIDs[0] = "A123";
+// fraudulentOrderIDs[1] = "B456";
+// fraudulentOrderIDs[2] = "C789";
+// // fraudulentOrderIDs[3] = "D000";
+
+string[] fraudulentOrderIDs = { "A123", "B456", "C789" };
+
+Console.WriteLine($"First: {fraudulentOrderIDs[0]}");
+Console.WriteLine($"Second: {fraudulentOrderIDs[1]}");
+Console.WriteLine($"Third: {fraudulentOrderIDs[2]}");
+
+fraudulentOrderIDs[0] = "F000";
+
+Console.WriteLine($"Reassign First: {fraudulentOrderIDs[0]}");
+
+Console.WriteLine($"There are {fraudulentOrderIDs.Length} fraudulent orders to process.");
+
+// foreach
+string[] names = { "Bob", "Conrad", "Grant" };
+foreach (string name in names)
+{
+    Console.WriteLine(name);
+}
+
+int[] inventory = { 200, 450, 700, 175, 250 };
+int sum = 0;
+int bin = 0;
+foreach (int items in inventory)
+{
+    sum += items;
+    bin++;
+    Console.WriteLine($"Bin {bin} = {items} items (Running total: {sum})");
+}
+Console.WriteLine($"We have {sum} items in inventory.");
+
+// project foreach
+string[] fakeOrderIDs = {"B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179"};
+
+foreach (string item in fakeOrderIDs)
+{
+    if (item.StartsWith("B"))
+    {
+        Console.WriteLine(item);
+    }
+}
