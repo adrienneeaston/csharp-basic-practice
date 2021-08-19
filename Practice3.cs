a753e9988d4042dfb9bc8aba84cd0bc099eb4440
Adrienne <adrienneeaston@gmail.com>
2021-08-18 23:59:23 -0700

    comments

---
+++ Practice3.cs
@@ -0,0 +1,12 @@
+Console.WriteLine("Hello World!");
+
+// This is a single line comment
+
+/*
+This is a multiple line comment.
+It has more than one line
+*/
+
+/// <summary>
+/// This is an XML documentation comment. 
+/// </summary>
